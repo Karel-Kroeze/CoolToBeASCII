d6047c15fb2575c0d3d4ddc5294aeb06c782359c
Karel Kroeze <karel.kroeze@gmail.com>
2020-02-25 15:04:15 +0100

    add font gallery

--- R/main.R
+++ R/main.R
@@ -3,19 +3,24 @@
 #' Uses http://artii.herokuapp.com/ API, so requires an active internet connection.
 #'
 #' @param text A string to be ASCII-fied
-#' @param font The font to use. See http://artii.herokuapp.com/fonts_list for a comprehensive list of available fonts.
+#' @param font The font to use
 #' @export
-#' @import httr utils
+#' @seealso AvailableFonts
 #' @examples
 #' ASCIIfy( "test" )
 ASCIIfy <- function( text, font = NULL ){
-    url <- paste0( "http://artii.herokuapp.com/make?text=", utils::URLencode( text ) )
+    url <- paste0( "https://artii.herokuapp.com/make?text=", utils::URLencode( text ) )
     if( !is.null( font ) ) url <- paste0( url, "&font=", utils::URLencode( font ) )
-    res <- httr::GET( url )
-    if (res$status_code != 200){
-        stop(res)
-    }
-    return( httr::content( res, encoding = "UTF-8" ) )
+    tryCatch({
+        res <- httr::GET( url )
+        if (res$status_code != 200){
+            warning( httr::content( res, encoding = "UTF-8" ), "\nAre you sure this font exists? See https://artii.herokuapp.com/fonts_list for available fonts." )
+        } else {
+            return( httr::content( res, encoding = "UTF-8" ) )
+        }
+    }, error = function( e ){
+        message( paste0( err$message, "\nIs an internet connection available?" ))
+    })
 }
 
 #' Prints a super cool ASCII title in the font of your choice
@@ -24,6 +29,7 @@ ASCIIfy <- function( text, font = NULL ){
 #'
 #' @param text A string to be ASCII-fied
 #' @param font The font to use. See http://artii.herokuapp.com/fonts_list for a comprehensive list of available fonts.
+#' @seealso AvailableFonts
 #' @export
 #' @examples
 #' CoolToBeASCII("Step 1: Do something")
@@ -31,5 +37,33 @@ ASCIIfy <- function( text, font = NULL ){
 #' CoolToBeASCII("Step 3: Profit", font = "starwars" )
 CoolToBeASCII <- function( text, font = NULL ){
     raw <- ASCIIfy( text, font )
-    cat( raw )
+    cat( raw, "\n" )
+}
+
+#' Get list of available fonts
+#'
+#' Returns a vector of available fonts.
+#' @export
+AvailableFonts <- function(){
+    url <- "https://artii.herokuapp.com/fonts_list";
+    res <- httr::GET( url );
+    return( strsplit( httr::content( res, encoding = "UTF-8" ), "\n" )[[1]] )
+}
+
+#' Preview fonts
+#'
+#' Note that there are large number of fonts, and many of them are less than
+#' practical.
+#' @export
+ASCIIFontGallery <- function(){
+    fonts <- GetFonts();
+    i <- 0;
+    for (font in fonts){
+        if ( i >= 3 ){
+            readline(prompt="Press [enter] to continue")
+            i <- 0
+        } else { i <- i + 1 }
+        cat( "\n\n", font, "\n" )
+        CoolToBeASCII( font, font )
+    }
 }
